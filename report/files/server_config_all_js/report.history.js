9c43b6e99c49406cbb9c5c50653e3f53f0760f2b
localnerve <alex@localnerve.com>
2013-10-21 00:21:47 -0400

    report update 10/21/2013

--- report/files/server_config_all_js/report.history.js
+++ report/files/server_config_all_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Fri, 18 Oct 2013 15:49:18 GMT","sloc":62,"lloc":29,"functions":1,"deliveredBugs":0.18192815379937763,"maintainability":100,"lintErrors":0,"difficulty":5.8125}]
+__history = [{"date":"Fri, 18 Oct 2013 15:49:18 GMT","sloc":62,"lloc":29,"functions":1,"deliveredBugs":0.18192815379937763,"maintainability":100,"lintErrors":0,"difficulty":5.8125},{"date":"Mon, 21 Oct 2013 04:20:35 GMT","sloc":62,"lloc":29,"functions":1,"deliveredBugs":0.18192815379937763,"maintainability":100,"lintErrors":0,"difficulty":5.8125}]
