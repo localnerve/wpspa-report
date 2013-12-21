257b82ee47b10465246e63e8a12b2b0b122f1404
localnerve <alex@localnerve.com>
2013-12-20 22:33:50 -0500

    Report Update 2013/12/20

---
+++ report/files/app_components_layout_header_banner_view_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Sat, 21 Dec 2013 03:33:07 GMT","sloc":31,"lloc":15,"functions":3,"deliveredBugs":0.16737107026397122,"maintainability":72.46489121009586,"lintErrors":0,"difficulty":10.214285714285715}]
