30796dd1c87879df607af1a4c812b0414f3bc0dc
localnerve <alex@localnerve.com>
2014-02-22 10:11:47 -0500

    Report Update 2014/02/22

---
+++ report/files/app_loaders_jst_dev_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Sat, 22 Feb 2014 15:04:19 GMT","sloc":41,"lloc":18,"functions":5,"deliveredBugs":0.20869041766052765,"maintainability":76.04719057257464,"lintErrors":0,"difficulty":17.5}]
